af5d38ef324f4860a6d4f9d2a1c218a29cc796f1
masterbuchi <33148960+masterbuchi@users.noreply.github.com>
2018-05-17 23:17:31 +0200

    Revert "Revert "Hallo""
    This reverts commit 8e284f70dbc51fee412be374ab1c0e2b942e10f6.

---
+++ js/Weapon.js
@@ -0,0 +1,21 @@
+class Weapon {
+            constructor(x, y, name, gravitation, bounce) {
+                this.x = x;
+                this.y = y;
+                this.name = name;
+                this.gravitation = gravitation;
+                this.bounce = bounce;
+
+                var waffe = game.add.sprite(this.x, this.y, this.name);
+                game.physics.arcade.enable(waffe);
+                waffe.body.gravity.y = this.gravitation;
+                waffe.body.bounce.y = this.bounce;
+                waffe.body.collideWorldBounds = true;
+                this.waffe = waffe;
+            }
+
+            getWeapon() {
+                return this.waffe;
+            }
+
+        }
