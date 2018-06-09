84e63c09c5a5b105aa808d6d7431f83a162a3292
masterbuchi <33148960+masterbuchi@users.noreply.github.com>
2018-06-09 10:58:30 +0200

    Playerklasse funktioniert, Waffenaufnahme wird implementiert

--- js/Player.js
+++ js/Player.js
@@ -1,6 +1,6 @@
 class Player extends Phaser.Sprite {
     constructor(game) {
-        super(game,0, 0, 'player');
+        super(game, 0, 0, 'player');
 
         this.exists = false;
         this.scale.setTo(0.5, 0.5);
@@ -8,7 +8,7 @@ class Player extends Phaser.Sprite {
         game.physics.arcade.enable(this);
         this.body.collideWorldBounds = true;
         this.body.gravity.y = 200;
-        
+
 
 
         this.schadenstypen = {
@@ -23,7 +23,7 @@ class Player extends Phaser.Sprite {
         this.animations.add('left', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], 15, true);
         this.animations.add('jump_right', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
             15, false);
-            this.animations.add('jump_left', [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
+        this.animations.add('jump_left', [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
             15, false);
 
 
