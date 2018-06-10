4e4f1f9467d1bc18f59eb48a68373ca2d631ede0
masterbuchi <33148960+masterbuchi@users.noreply.github.com>
2018-06-11 00:54:55 +0200

    Waffen-Klasse angepasst

--- js/Waffe.js
+++ js/Waffe.js
@@ -1,15 +1,22 @@
-class Waffe extends Phaser.Sprite{
-    
-    constructor(game, x, y, name, gravitation, bounce) {
-        super(game, x, y, name);
+class Waffe extends Phaser.Sprite {
+
+    constructor(game, gravitation, bounce) {
+
+        super(game, 0, 0, 'schwacherGegner');
+        this.exists = false;
+        game.physics.arcade.enable(this);
+        this.body.collideWorldBounds = true;
         this.body.gravity.y = gravitation;
         this.body.bounce.y = bounce;
-        this.body.collideWorldBounds = true;
-        game.physics.arcade.enable(this);
-
-
-
-
     }
 
+    stdReset(x, y) {
+        this.reset(x, y);
+        this.exists = true;
+    }
+
+    spawn(x, y, type) {
+        this.stdReset(x, y);
+        this.loadTexture(type);
+    }
 }
